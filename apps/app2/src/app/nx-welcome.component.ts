a4f866b6d8b911ecf572ffd24d68b38c1df61f2b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-05-08 14:08:34 +0200

    Preparo 2 aplicaciones con NX

--- apps/app2/src/app/nx-welcome.component.ts
+++ apps/app2/src/app/nx-welcome.component.ts
@@ -1,10 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'nx-test-nx-welcome',
-  standalone: true,
-  imports: [CommonModule],
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -423,7 +420,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-           Hola ke ase👋
+            Hola ke ase app2 👋
           </h1>
         </div>
         <!--  HERO  -->

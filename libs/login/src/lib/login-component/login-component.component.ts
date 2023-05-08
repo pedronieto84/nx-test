cf7301ef0f6b3a902599d4990a55943103a76729
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-05-08 14:17:42 +0200

    Creo componente login

---
+++ libs/login/src/lib/login-component/login-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-test-login-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.scss'],
+})
+export class LoginComponentComponent {}

5e700fd767a3030168c704aa1050ad9b3cae85e4
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-05-24 14:03:08 +0200

    linting forcing error

--- apps/app1/src/app/nx-welcome.component.ts
+++ apps/app1/src/app/nx-welcome.component.ts
@@ -1,12 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponentComponent} from '../../../../libs/login/src/index'
 
 @Component({
   selector: 'nx-test-nx-welcome',
   standalone: true,
-  imports: [CommonModule, LoginComponentComponent],
-  template: `<div>Hola app1</div><nx-test-login-component></nx-test-login-component>
+  imports: [CommonModule],
+  template: `<div>Hola app1</div>
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
